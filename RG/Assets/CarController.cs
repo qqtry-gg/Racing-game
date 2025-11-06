a0878b033a7ce8f43bed04c3d2ff84a893d19051
qqtry-gg <wojtek.bielak1@interia.pl>
2025-11-06 19:53:50 +0100

    added steering system and brake system

--- RG/Assets/CarController.cs
+++ RG/Assets/CarController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+
     public WheelCollider FRwheel;
     public WheelCollider FLwheel;
     public WheelCollider BRwheel;
@@ -15,21 +17,36 @@ public class CarController : MonoBehaviour
 
     public float gasInput;
     public float steeringInput;
+    public float brakepower;
+    private float slipAngle;
+    public float brakeInput;
 
     public float motorPower;
+    float speed;
+    public AnimationCurve steeringCurve;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRB = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = playerRB.linearVelocity.magnitude;
         CheckInput();
         ApplyWheelPositions();
         ApplyMotor();
+        ApplySteering();
+        ApplyBreak();
+    }
+    void ApplyBreak()
+    {
+        FRwheel.brakeTorque = brakeInput * brakepower * 0.7f;
+        FLwheel.brakeTorque = brakeInput * brakepower * 0.7f;
+        BRwheel.brakeTorque = brakeInput * brakepower * 0.3f;
+        BLwheel.brakeTorque = brakeInput * brakepower * 0.3f;
     }
     void ApplyWheelPositions()
     {
@@ -37,6 +54,12 @@ public class CarController : MonoBehaviour
         UpdateWheel(FLwheel, FLwheelMesh);
         UpdateWheel(BRwheel, BRwheelMesh);
         UpdateWheel(BLwheel, BLwheelMesh);
+    }
+    void ApplySteering()
+    {
+        float steeringangle = steeringInput * steeringCurve.Evaluate(speed);
+        FRwheel.steerAngle= steeringangle;
+        FLwheel.steerAngle = steeringangle;
     }
     void UpdateWheel(WheelCollider collider, MeshRenderer renderer)
     {
@@ -51,6 +74,23 @@ public class CarController : MonoBehaviour
     {
         gasInput = Input.GetAxis("Vertical");
         steeringInput = Input.GetAxis("Horizontal");
+        slipAngle = Vector3.Angle(transform.forward, playerRB.angularVelocity - transform.forward); //it might have to be linearVelocity this might be my mistake
+        if (slipAngle < 120f)
+        {
+            if (gasInput < 0)
+            {
+                brakeInput = Mathf.Abs(gasInput);
+                gasInput = 0;
+            }
+            else
+            {
+                brakeInput = 0;
+            }
+        }
+        else
+        {
+            brakeInput = 0;
+        }
     }
     void ApplyMotor()
     {
