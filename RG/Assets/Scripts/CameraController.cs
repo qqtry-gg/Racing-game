52bcdf6b0cc2fffb7b6cde9efd4b92c6a83f8160
qqtry-gg <wojtek.bielak1@interia.pl>
2025-11-07 20:16:07 +0100

    added diffrent cars

--- RG/Assets/Scripts/CameraController.cs
+++ RG/Assets/Scripts/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Car").transform;
         playerRB = player.GetComponent<Rigidbody>();
     }
 
